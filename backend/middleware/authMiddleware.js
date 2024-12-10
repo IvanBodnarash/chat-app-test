67be2a379ed14871bac2f1af2fdc7ec481f5d697
unknown <bodnarash1999@gmail.com>
2024-12-10 23:33:59 +0100

    Update back 5

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 export const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
+  if (req.session && req.session.passport && req.session.passport.user) {
     return next();
   }
   res.status(401).json({ message: "Not authenticated" });
