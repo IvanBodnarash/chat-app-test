901e3328669817d1fcf22b83a342568dd3733602
unknown <bodnarash1999@gmail.com>
2024-12-11 15:30:22 +0100

    Refactoring 1.0

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,19 @@ import dotenv from "dotenv";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-// import session from "express-session";
 import { connect } from "mongoose";
 import cors from "cors";
-// import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
 
 import fetch from "node-fetch";
-import https from "https";
+// import https from "https";
 import chatRoutes from "./routes/chatRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 
 import Chat from "./models/Chat.js";
 import Message from "./models/Message.js";
 
-const agent = new https.Agent({ rejectUnauthorized: false });
+// const agent = new https.Agent({ rejectUnauthorized: false });
 
 dotenv.config();
 
@@ -41,63 +39,15 @@ app.use(
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    // allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );  
 app.use(express.json());
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       secure: true,
-//       httpOnly: true,
-//       sameSite: "none",
-//       maxAge: 24 * 60 * 60 * 1000,
-//     },
-//     store: MongoStore.create({
-//       mongoUrl: process.env.MONGODB_URI,
-//     }),
-//   })
-// );
-
 app.use(cookieParser());
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// Use session in WebSockets
-// io.use(
-//   passportSocketIo.authorize({
-//     cookieParser: cookieParser,
-//     key: "connect.sid",
-//     secret: process.env.SESSION_SECRET,
-//     store: sessionStore,
-//     success: (data, accept) => {
-//       console.log("WebSocket session established:", data);
-//       accept(null, true);
-//     },
-//     fail: (data, message, error, accept) => {
-//       console.error("WebSocket session failed. Reason:", message);
-//       console.error("Headers received:", data.headers);
-//       accept(null, false);
-//     },
-//   })
-// );
 
 // Routes
-// app.use("/auth", authRoutes);
 app.use("/chats", chatRoutes);
 app.use("/messages", messageRoutes(io));
-
-// app.use((req, res, next) => {
-//   console.log("Session ID:", req.sessionID);
-//   console.log("Session Data:", req.session);
-//   console.log("Session in middleware:", req.session);
-//   console.log("User in middleware:", req.user);
-//   next();
-// });
 
 app.get("/", (req, res) => {
   res.send("WebSocket server is running!");
@@ -105,8 +55,6 @@ app.get("/", (req, res) => {
 
 // Websocket logic
 io.on("connection", (socket) => {
-  // console.log("New WebSocket connection");
-  // console.log("Session ID:", socket.request.sessionID);
   // Start auto-generated messages
   socket.on("startAutoMessages", async () => {
     if (isAutoMessagesRunning) {
@@ -123,9 +71,7 @@ io.on("connection", (socket) => {
         const randomChat = chats[Math.floor(Math.random() * chats.length)];
 
         // Get random quote from Quotable
-        const quoteRsponse = await fetch("https://api.quotable.io/random", {
-          agent,
-        });
+        const quoteRsponse = await fetch("https://api.quotable.io/random");
         const quote = await quoteRsponse.json();
 
         const randomMessage = `Auto-generated message: "${quote.content}" - ${quote.author}`;
@@ -187,8 +133,6 @@ connect(process.env.MONGODB_URI)
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
   });
-
-// console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 // Start the server
 const PORT = process.env.PORT || 5001;
